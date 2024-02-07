65f1be65992546f92b82f5b640543260373e2ee1
Julian <Julian@DESKTOP-IVUAG9U>
2024-02-08 00:19:49 +0100

    Add project files.

---
+++ opdracht1.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+double berekenBelasting(double inkomen) {
+    double belasting = 0.0;
+    if (inkomen <= 75518) {
+        belasting = inkomen * 0.3697;
+    }
+    else {
+        belasting = 75518 * 0.3697 + (inkomen - 75518) * 0.4950;
+    }
+    return belasting;
+}
+
+int main() {
+    double inkomen;
+    printf("Voer uw jaarinkomen in: ");
+    scanf_s("%lf", &inkomen);
+
+    double belasting = berekenBelasting(inkomen);
+    printf("De inkomstenbelasting op een inkomen van %.2f EURO is %.2f\n EURO", inkomen, belasting);
+
+    return 0;
+}
